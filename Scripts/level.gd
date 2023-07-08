0d02ece18de7ab7e2fa9ce26dcaa473999143122
ckuras <kuras.chris@gmail.com>
2023-07-08 15:20:22 -0400

    Connected winning with level change

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -5,5 +5,5 @@ signal level_changed(level_name)
 @export var level_number: int = 0
 
 func _ready():
-	await get_tree().create_timer(1.0).timeout
+	await EventBus.player_won_round
 	emit_signal("level_changed", "level_" + str(level_number + 1))
