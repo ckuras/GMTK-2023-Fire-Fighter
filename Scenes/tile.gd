90e2cbaf3fb4c60097f8904f5550aa1e7ee3e980
ckuras <kuras.chris@gmail.com>
2023-07-07 20:27:46 -0400

    added inversion to modulate

--- Scenes/tile.gd
+++ Scenes/tile.gd
@@ -9,8 +9,14 @@ enum TileState {
 
 func _ready(): 
 	print("i am a tile: ", tile_state_to_string(state))
-
+	modulate = state_to_modulate(state)
+	
 func tile_state_to_string(tile_state: TileState):
 	match tile_state:
 		0: return "None"
 		1: return "Fire"
+
+func state_to_modulate(tile_state: TileState):
+	match tile_state:
+		0: return modulate
+		1: return modulate.inverted()
