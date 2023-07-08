5c90d75e76d45c248b36279d3c2981ca4469e139
Christian Bouwense <bouwensec@gmail.com>
2023-07-08 11:12:01 -0400

    reset moves on player turn (#10)

--- Scripts/PlayerTurn.gd
+++ Scripts/PlayerTurn.gd
@@ -2,5 +2,10 @@ extends Turn
 
 func play_turn():
 	print("Player's turn")
+	reset_player_moves_remaining()
+	
 	await EventBus.turn_ended
 	emit_signal("completed")
+
+func reset_player_moves_remaining():
+	game_state.player_moves_remaining = 2
