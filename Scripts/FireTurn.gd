2e4299ecd49531b4bb47ab02114ee5c99a08aeb9
ckuras <kuras.chris@gmail.com>
2023-07-08 11:28:15 -0400

    Set game state turns in play turns
    restrict player movement to player turn

--- Scripts/FireTurn.gd
+++ Scripts/FireTurn.gd
@@ -2,6 +2,7 @@ extends Turn
 
 func play_turn():
 	print("Fire's turn")
+	game_state.active_turn = game_state.Turn.Fire
 	var tiles_on_fire: Array[Tile] = board.get_tiles_by_state(Tile.TileState.Fire)
 	await get_tree().create_timer(1.0).timeout
 	for tile in tiles_on_fire:
