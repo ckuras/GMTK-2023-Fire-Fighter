202d3998e7b99b3815bd6a551c58c31ed47976c6
ckuras <kuras.chris@gmail.com>
2023-07-08 09:40:30 -0400

    Added plant/infected turn

--- Scripts/TurnQueue.gd
+++ Scripts/TurnQueue.gd
@@ -4,6 +4,7 @@ extends Node
 @export var game_state: GameState
 
 func _ready():
+	game_state.active_turn = game_state.Turn.Infected
 	play_turn()
 
 func play_turn():
@@ -17,4 +18,14 @@ func play_turn():
 			await get_tree().create_timer(1.0).timeout
 			for tile in tiles_on_fire:
 				tile.spread_to_neighbors()
+			game_state.active_turn = game_state.Turn.Infected
 			play_turn()
+		game_state.Turn.Infected:
+			print("Infected's turn")
+			var tiles_on_fire: Array[Tile] = board.get_tiles_by_state(Tile.TileState.Infected)
+			await get_tree().create_timer(1.0).timeout
+			for tile in tiles_on_fire:
+				tile.spread_to_neighbors()
+			game_state.active_turn = game_state.Turn.Fire
+			play_turn()
+			
